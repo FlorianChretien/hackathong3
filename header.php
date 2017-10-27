107e7071447cd4d167bdd28f20fe368d1067cebb
ThibMar <thibmard@gmail.com>
2017-10-27 07:52:33 +0200

    header MAJ
    rajout du menu burger, et liens profil

--- header.php
+++ header.php
@@ -5,9 +5,39 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style/style.css" />
+    <link href="css/style-thibaut.css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Kalam:300,400,700|Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
     <title>Musea Hero</title>
 </head>
 <body>
-<h1 id="alert-pc">Ce site n'est disponible que pour mobile.</h1>
+	<header>
+            
+<!-- Burger menu, merci Twikito ! :) -->
+    	<input type="checkbox" id="navcheck" role="button" title="menu" class="input-menu">
+        <label for="navcheck" aria-hidden="true" title="menu">
+            <span class="burger">
+                <span class="bar">
+                    <span class="visuallyhidden"></span>
+                </span>
+            </span>
+        </label>
+        <nav id="menu">
+            <a href="#">#</a>
+            <a href="#">#</a>
+            <a href="#">#</a>
+            <a href="#">#</a>
+            <a href="#">#</a>
+            <a href="#">#</a>
+        </nav>
+    
+    
+        <a href="#" class="link-fix">
+            
+            <img src="img/asset_cta/profil.png">
+            
+        </a>
+            
+    </header>
+	<h1 id="alert-pc">Ce site n'est disponible que pour mobile.</h1>
 
