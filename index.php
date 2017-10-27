394f96ce8fc19b7d39cb45d23904877d25f1e095
FlorianChretien <floindrix36@gmail.com>
2017-10-27 11:32:33 +0200

    dsf

--- index.php
+++ index.php
@@ -2,19 +2,21 @@
 include_once('header.php');
 ?>
 
-<p>Je suis </p>
-<select id="lieu">
-    <option value="0">...</option>
-    <option value="1">chez moi</option>
-    <option value="2">dans un musée</option>
-</select>
-<div id="bloc-musee">
-    <p>Super ! Entre le nom ou le code de ton musée :</p>
-    <form action="function.php" method="POST">
-        <input name="code" placeholder="..." />
-        <input type="submit" />
-    </form>
-</div>
+<section id="section-index">
+    <p>Je suis </p>
+    <select id="lieu">
+        <option value="0">...</option>
+        <option value="1">chez moi</option>
+        <option value="2">dans un musée</option>
+    </select>
+    <div id="bloc-musee">
+        <p>Super ! Entre le nom ou le code de ton musée :</p>
+        <form action="function.php" method="POST">
+            <input name="code" placeholder="..." />
+            <input type="submit" />
+        </form>
+    </div>
+</section>
 
 <?php
 
