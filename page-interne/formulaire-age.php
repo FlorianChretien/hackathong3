f7268f9fc31c18b86afa27292c0a5a51237e2ea3
ThibMar <thibmard@gmail.com>
2017-10-27 11:56:03 +0200

    yolo

---
+++ page-interne/formulaire-age.php
@@ -0,0 +1,23 @@
+<?php
+include_once('../header.php');
+?>
+
+	 <div class="container-home">
+        <input type="text" name="pseudo" placeholder="Pseudo">
+        <input type="password" name="mot de passe" placeholder="Mot de passe">
+        
+        <div class="pointed">  
+            <a href="#" class="btn-rounded-pointed">entrer</a>
+        </div> 
+        
+        <p class="texte">ou</p>
+        
+        <div class="pointed"> 
+            <a href="#" class="btn-rounded-pointed">créer un compte</a>
+        </div>
+    </div>    
+    
+
+<?php
+include_once('../footer.php');
+?>
