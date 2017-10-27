ad1334ff91d38556f02d81d5253590065aaeae81
ThibMar <thibmard@gmail.com>
2017-10-27 15:03:23 +0200

    jhfd

--- page-interne/profil.php
+++ page-interne/profil.php
@@ -2,6 +2,36 @@
 include_once('../header.php');
 ?>
 
+
+
+
+<section class="container-profil">
+
+	<img src="../img/asset_cta/profil.png">
+
+	    
+
+	    <a href="#" class="pointed-contain-1">
+	    	
+	    	<img src="../img/icone/boussole-01.png">
+	    	<p>quête accomplie</p>
+
+	    </a>
+	    <a href="#" class="pointed-contain-2">
+	    	
+	    	<img src="../img/icone/boussole-02.png">
+	    	<p>quête échouée</p>
+
+	    </a>
+	    <a href="#" class="pointed-contain-3">
+	    	
+	    	<img src="../img/icone/trophe-01.png">
+	    	<p>récompences</p>
+
+	    </a>
+	</section>
+
+
 <?php
 include_once('../footer.php');
 ?>
